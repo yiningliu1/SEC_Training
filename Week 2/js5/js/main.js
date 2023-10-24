61087becb42cfbe8f3f96123aa2710dd86cc1c60
yiningliu1 <145304243+yiningliu1@users.noreply.github.com>
2023-10-23 23:31:44 -0400

    finished chapter 5

---
+++ Week 2/js5/js/main.js
@@ -0,0 +1,15 @@
+console.log(Math.PI);
+console.log(Math.trunc(Math.PI));
+console.log(Math.ceil(3.4));
+console.log(Math.floor(Math.PI));
+
+console.log(Math.pow(2,5));
+
+console.log(Math.min(2, 0.5, 9));
+console.log(Math.max(2, 9.2, 9));
+
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
