ffc379eb03083ba8d69de27c46cf6f94627c0a81
yiningliu1 <145304243+yiningliu1@users.noreply.github.com>
2023-10-26 23:11:30 -0400

    finished chapter 11

--- Week 2/js9/js/main.js
+++ Week 2/js9/js/main.js
@@ -1,8 +1,1 @@
-let name = prompt("Please enter your name.");
-if (name) {
-  console.log(name.length);
-  console.log(name.trim().length)
-  console.log(name.trim());
-} else {
-  console.log("You didn't enter your name.");
-}
+let playGame = confirm("Shall we play rock, paper, or scissors?");
