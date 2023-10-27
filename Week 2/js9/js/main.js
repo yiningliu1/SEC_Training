2577a030403f828a86f0fe3ca0072ad36d4d0a4b
yiningliu1 <145304243+yiningliu1@users.noreply.github.com>
2023-10-26 22:27:58 -0400

    finished chapter 10

---
+++ Week 2/js9/js/main.js
@@ -0,0 +1,8 @@
+let name = prompt("Please enter your name.");
+if (name) {
+  console.log(name.length);
+  console.log(name.trim().length)
+  console.log(name.trim());
+} else {
+  console.log("You didn't enter your name.");
+}
