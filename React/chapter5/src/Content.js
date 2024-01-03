9f8e6065e921e57f59a8fddbd37a7d61df7cb126
yiningliu1 <145304243+yiningliu1@users.noreply.github.com>
2024-01-03 13:10:43 -0500

    finished chapter 5

---
+++ React/chapter5/src/Content.js
@@ -0,0 +1,32 @@
+const Content = () => {
+  const handleNameChange = () => {
+    const names = ['Bob', 'Kevin', 'Dave'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+
+  const handleClick = () => {
+    console.log('You clicked it');
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  }
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  }
+
+  return (
+    <main>
+      
+    <p onDoubleClick={handleClick}>
+      Hello {handleNameChange()}!
+    </p>
+    <button onClick={handleClick}>Click It</button>
+    <button onClick={() => handleClick2('Dave')}>Click It</button>
+    <button onClick={(e) => handleClick3(e)}>Click It</button>
+    </main>
+  )
+}
+
+export default Content
